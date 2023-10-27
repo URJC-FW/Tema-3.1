a95aa25e70ef49099d35d8d0651b4e4d4423d629
ivchicano <ivchicano@gmail.com>
2023-10-27 10:39:03 +0200

    fix exercise 6 having wrong variable names in loops

--- Ejercicio6/app.js
+++ Ejercicio6/app.js
@@ -1,6 +1,6 @@
 class Rectangulo {
 
-    constructor(color, alto, ancho){
+    constructor(color, alto, ancho) {
         this.color = color;
         this.alto = alto;
         this.ancho = ancho;
@@ -9,59 +9,59 @@ class Rectangulo {
     area() {
         return this.alto * this.ancho
     }
-    
+
     perimetro() {
         return this.alto * 2 + this.ancho * 2
     }
 
-    toString(){
-        return 'area:'+this.area()+' perimetro:'+this.perimetro();
+    toString() {
+        return 'area:' + this.area() + ' perimetro:' + this.perimetro();
     }
 }
 
 class AnalizadorFiguras {
 
-    constructor(figuras){
+    constructor(figuras) {
         this.figuras = figuras;
     }
 
-    sumaAreas(){
+    sumaAreas() {
 
         let sumaAreas = 0;
-        
-        for(let i=0; i<rects.length; i++){
-            let rect = rects[i];
+
+        for (let i = 0; i < this.figuras.length; i++) {
+            let rect = this.figuras[i];
             sumaAreas += rect.area();
         }
-        
+
         return sumaAreas;
     }
 
-    sumaPerimetros(){
+    sumaPerimetros() {
 
         let sumaPerimetros = 0;
-        
-        for(let i=0; i<rects.length; i++){
-            let rect = rects[i];
+
+        for (let i = 0; i < this.figuras.length; i++) {
+            let rect = this.figuras[i];
             sumaPerimetros += rect.perimetro();
         }
-        
+
         return sumaPerimetros;
     }
 
-    areaMedia(){
+    areaMedia() {
         return this.sumaAreas() / this.figuras.length;
     }
 
-    perimetroMedio(){
+    perimetroMedio() {
         return this.sumaPerimetros() / this.figuras.length;
     }
 }
 
 //------------------------------------
 
-let rects = [ 
-    new Rectangulo('verde', 5, 6), 
+let rects = [
+    new Rectangulo('verde', 5, 6),
     new Rectangulo('rojo', 2, 3),
     new Rectangulo('amarillo', 5, 5),
     new Rectangulo('negro', 10, 5)
